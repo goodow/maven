e71741c42aec00f56777b2409ea6b73e31846ae5
Larry Tin <i@goodow.com>
2013-05-30 09:32:07 +0800

    Add realtime-parent deploy script

---
+++ repositories/deploy-realtime.sh
@@ -0,0 +1,4 @@
+mvn deploy:deploy-file \
+-Durl=file:///Users/retechretech/dev/workspace/maven/repositories/android \
+-DpomFile=/Users/retechretech/dev/workspace/realtime/pom.xml \
+-Dfile=/Users/retechretech/dev/workspace/realtime/pom.xml
