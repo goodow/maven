90087bf82b6e32fd193137a5219d527742e36343
Larry Tin <i@goodow.com>
2013-05-31 16:08:24 +0800

    Add diff_match_patch

--- repositories/deploy-realtime.sh
+++ repositories/deploy-realtime.sh
@@ -1,4 +1,5 @@
 mvn deploy:deploy-file \
--Durl=file:///Users/retechretech/dev/workspace/maven/repositories/android \
+-Durl=file:///Users/retechretech/dev/workspace/maven/repositories/realtime \
 -DpomFile=/Users/retechretech/dev/workspace/realtime/pom.xml \
 -Dfile=/Users/retechretech/dev/workspace/realtime/pom.xml
+# -Dsources=
