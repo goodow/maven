dab36f096ae3ac8092e8109d0a1027b27e8be62b
larry <i@goodow.com>
2014-02-12 11:19:25 +0800

    Add baidu maps geosdk

--- repositories/deploy-realtime.sh
+++ repositories/deploy-realtime.sh
@@ -1,3 +1,5 @@
+mvn install:install-file -Dfile=locSDK_4.0.jar -DgroupId=com.baidu.maps.android -DartifactId=maps-geosdk -Dversion=4.0 -Dpackaging=jar
+
 mvn deploy:deploy-file \
 -Durl=file:///Users/retechretech/dev/workspace/maven/repositories/realtime \
 -DpomFile=/Users/retechretech/dev/workspace/realtime/pom.xml \
